3ef81cf454350d71a2d7744d06b43814254c2468
Akralan <alix.chagot@gmail.com>
2026-02-11 18:56:09 -0300

    Adding Service for Google Workspace

---
+++ packages/services/google-workspace/index.ts
@@ -0,0 +1,3 @@
+export { GoogleAuthService } from './google-auth.service.js';
+export { GoogleCalendarService } from './google-calendar.service.js';
+export { GoogleMeetService } from './google-meet.service.js';
