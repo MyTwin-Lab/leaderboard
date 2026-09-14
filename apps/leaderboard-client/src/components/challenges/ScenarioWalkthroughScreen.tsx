6f88982af18d204373ac66b23defd7f9e6d815ff
Akralan <alix.chagot@gmail.com>
2026-09-14 09:46:10 +0200

    feat(validation): show a validator which applications are waiting

---
+++ apps/leaderboard-client/src/components/challenges/ScenarioWalkthroughScreen.tsx
@@ -0,0 +1,7 @@
+'use client';
+export function ScenarioWalkthroughScreen(_props: {
+  challengeId: string; contributionId: string; submitterName: string;
+  endpointUrl: string | null; onClose: () => void;
+}) {
+  return null;
+}
